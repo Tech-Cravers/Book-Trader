d9cc836710a06187e06d2334202055aff2b50940
RushilVerma <rushilverma07@gmail.com>
2021-10-24 11:43:19 +0530

    folder management

--- test_npm/app.js
+++ test_npm/app.js
@@ -1,6 +1,65 @@
+console.log("Running app.js");
+
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const multer = require('multer');
+const { createWorker } = require("tesseract.js");
+const worker = createWorker({
+  logger: m => console.log(m),
+});
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req,file,cb) => {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage: storage}).single("fileName");
+
+app.set('views', '../')
+app.set("view engine" , "ejs");
+app.set(express.static("public"));
+
+//ROUTES
+app.get('/',(req,res) => {
+    res.render('upload');
+});
+
+app.post('/uploads',(req,res) => {
+    upload(req,res, err => {
+        fs.readFile(`./uploads/${req.file.originalname}`,(err,data) => {
+            if (err) return console.log('ERROR : ',err);
+                (async () => {
+                await worker.load();
+                await worker.loadLanguage('eng');
+                await worker.initialize('eng');
+                const { data: { text } } = await worker.recognize(data);
+                console.log(text);
+                //res.send(text);
+                res.redirect('/download');
+                await worker.terminate();
+                })();
+                
+        });
+    });
+});
+
+app.get('/download', (req,res) => {
+    const file = `${__dirname}/uploads/wishes.png`;
+    res.download(file);
+})
+
+//starting the server
+const PORT = 5000|| process.env.PORT;
+app.listen(PORT, () => console.log(`Hey Im running on port ${PORT}`));
+
+/*
+//drive logic starts here 
 const { google } = require('googleapis') ;
 const path = require('path')
-const fs = require('fs')
+//const fs = require('fs')
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -80,4 +139,5 @@ async function linkgen(){
 }
 //uploadFile();
 //deleteFile();
-linkgen();
+//linkgen();
+*/
